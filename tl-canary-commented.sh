15736fea32f2f98ce2d32897015b56ff341e09d4
Tank Linux <tanklinux@protonmail.com>
2023-08-02 10:22:43 -0400

    08/02/23 10:22:43

--- tl-canary-commented.sh
+++ tl-canary-commented.sh
@@ -31,7 +31,6 @@ fi
 # Warn user about potential data loss
 dialog --defaultno --title "HIC SUNT DRACONES" --yesno "DANGER! HERE BE DRAGONS\n\nSelecting < Yes > will destroy the contents of: \n\n$selected_device_path"  10 60 || exit
 
-# Download necessary installation files
 curl -LO tanklinux.com/barbs.sh
 curl -LO tanklinux.com/tank-programs.csv
 curl -LO tanklinux.com/ascii-tank
@@ -57,50 +56,47 @@ while true; do
 	rootpass2=$(dialog --no-cancel --passwordbox "Retype the passphrase." 10 60 3>&1 1>&2 2>&3 3>&1)
 done
 
-# Provide option to write zeros across block device
+# Disk partitioning, formatting, and mounting
+
 dialog --defaultno --title "HIC SUNT DRACONES" --yesno "\nATTENTION HACKERMAN:\n\nSelect < Yes > to commence a lengthy process of writing zeros across the entirety of:\n\n$selected_device_path" 15 60 && dd if=/dev/zero of=$selected_device_path bs=1M status=progress || echo "Let's continue then..."
 
-# Get encryption passphrase
 luks_pass1=$(dialog --no-cancel --passwordbox "Enter a passphrase for the LUKS encryption.\n\nMake it unique, and write it down." 10 60 3>&1 1>&2 2>&3 3>&1)
 luks_pass2=$(dialog --no-cancel --passwordbox "Retype the encryption passphrase." 10 60 3>&1 1>&2 2>&3 3>&1)
+
 while true; do
     [[ "$luks_pass1" != "" && "$luks_pass1" == "$luks_pass2" ]] && break
     luks_pass1=$(dialog --no-cancel --passwordbox "Uh oh! Your passphrases do not match. Try again." 10 60 3>&1 1>&2 2>&3 3>&1)
     luks_pass2=$(dialog --no-cancel --passwordbox "Retype the passphrase." 10 60 3>&1 1>&2 2>&3 3>&1)
 done
 
-# Get disk setup packages
 pacman -S parted cryptsetup lvm2 --noconfirm
 
-# Create partitions
 parted -s -a optimal $selected_device_path mklabel msdos
+
 parted -s -a optimal $selected_device_path mkpart primary fat32 1MiB 1GiB
 
-# Determine if the block device is nvme or not
 if [[ $selected_device_path =~ [0-9]$ ]]; then
     boot_partition="${selected_device_path}p1"
 else
     boot_partition="${selected_device_path}1"
 fi
 
-# Setup the boot partition
 mkfs.fat -F32 "$boot_partition"
 parted -s $selected_device_path set 1 boot on
+
 parted -s -a optimal $selected_device_path mkpart primary 1GiB 100%
 
-# Setup the encryption partition
 if [[ $selected_device_path =~ [0-9]$ ]]; then
     luks_partition="${selected_device_path}p2"
 else
     luks_partition="${selected_device_path}2"
 fi
 
-# Check if LUKS container already exists
 luks_container_exists=$(cryptsetup isLuks "$luks_partition" && echo "yes" || echo "no")
 
-# Prompt user to proceed to destroy extant LUKS setup or bail out
 if [ "$luks_container_exists" = "yes" ]; then
     dialog --defaultno --title "LUKS Container Exists" --yesno "\nYou have an extant LUKS superblock signature on ${luks_partition}.\n\nSelect < Yes > to abort installation.\n\nSelect < No > to proceed, allowing the installation process to remove it. This will take ~ 10s" 15 60 && exit || batch_mode_flag="-q"
+
     if cryptsetup status tankluks >/dev/null 2>&1; then
         echo "Removing existing tankluks mapping..."
         cryptsetup remove tankluks
@@ -108,7 +104,6 @@ if [ "$luks_container_exists" = "yes" ]; then
 else
     batch_mode_flag=""
 fi
-
 
 echo -n "$luks_pass1" | cryptsetup ${batch_mode_flag} luksFormat "$luks_partition" -
 echo -n "$luks_pass1" | cryptsetup open --type luks "$luks_partition" tankluks -
@@ -130,6 +125,7 @@ mount "$boot_partition" /mnt/boot
 lsblk -f
 sleep 10s
 
+# Bootstrap the base system
 basestrap /mnt base base-devel runit elogind-runit linux linux-firmware vim neovim grub btrfs-progs dosfstools brightnessctl htop cryptsetup lvm2 lvm2-runit efibootmgr
 
 echo "basestrap ran"
@@ -152,6 +148,7 @@ echo "tankluks UUID=$encrypted_partition_uuid none luks" >> /mnt/etc/crypttab
 cp barbs.sh /mnt/root/
 cp tank-programs.csv /mnt/root/
 
+# Chroot into the new system
 artix-chroot /mnt /bin/bash <<EOF
 
 echo "Still have Encrypted partition UUID: $encrypted_partition_uuid"
@@ -166,19 +163,23 @@ sed -i "s|^GRUB_CMDLINE_LINUX_DEFAULT=\".*\"|GRUB_CMDLINE_LINUX_DEFAULT=\"loglev
 
 grub-install "$selected_device_path"
 grub-mkconfig -o /boot/grub/grub.cfg
+# Set root password, time, locale, and hostname
 
 echo "root:$rootpass1" | chpasswd
 
 hwclock --systohc
 
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
+# Setup network and time services
 echo "$hostname" > /etc/hostname
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
+# Install full xorg
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 export "LANG=en_US.UTF-8"
+# (Optional) Btrfs settings for Timeshift (currently commented out)
 echo "LC_COLLATE=C" >> /etc/locale.conf
 export LC_COLLATE="C"
 
@@ -188,13 +189,16 @@ ln -s /etc/runit/sv/NetworkManager/ /etc/runit/runsvdir/current
 pacman -S openntpd openntpd-runit --noconfirm
 ln -s /etc/runit/sv/openntpd/ /etc/runit/runsvdir/current
 
+# Execute barbs.sh script
 pacman -Sy xorg --noconfirm
 echo "full xorg install or reinstall"
+# Display post-installation message
 sleep 3s
 
 # btrfs quota enable /
 
 # sed -i \
+# Display ASCII art of tank
 #     -e 's/"do_first_run" : "true"/"do_first_run" : "false"/' \
 #     -e 's/"btrfs_mode" : "false"/"btrfs_mode" : "true"/' \
 #     -e 's/"include_btrfs_home" : "false"/"include_btrfs_home" : "true"/' \
--- tl-canary-further-commented.sh
+++ tl-canary-further-commented.sh
@@ -24,6 +24,7 @@ if [ -n "$selected_device" ]; then
     selected_device_path=$selected_device
     echo "Selected block device: $selected_device_path"
 else
+# Download necessary files from tanklinux.com
     echo "No block device was selected."
     exit
 fi
@@ -31,9 +32,9 @@ fi
 # Warn user about potential data loss
 dialog --defaultno --title "HIC SUNT DRACONES" --yesno "DANGER! HERE BE DRAGONS\n\nSelecting < Yes > will destroy the contents of: \n\n$selected_device_path"  10 60 || exit
 
-# Download necessary installation files
 curl -LO tanklinux.com/barbs.sh
 curl -LO tanklinux.com/tank-programs.csv
+# Collect user input for root passphrase
 curl -LO tanklinux.com/ascii-tank
 curl -LO tanklinux.com/README.md
 
@@ -42,6 +43,7 @@ hostname=$(dialog --stdout --no-cancel --inputbox "Enter a hostname for your sys
 
 # Set system's time zone
 if dialog --defaultno --title "Set your system's time zone." --yesno "\nDo you want to set the time zone to something other than Eastern time: America/New_York ?\n\nSelect yes to choose a different time zone."  10 60
+# Prepare disks for installation
 then
     timezone=$(tzselect)
 else
@@ -57,50 +59,49 @@ while true; do
 	rootpass2=$(dialog --no-cancel --passwordbox "Retype the passphrase." 10 60 3>&1 1>&2 2>&3 3>&1)
 done
 
-# Provide option to write zeros across block device
+# Disk partitioning, formatting, and mounting
+
+# Install the base system
 dialog --defaultno --title "HIC SUNT DRACONES" --yesno "\nATTENTION HACKERMAN:\n\nSelect < Yes > to commence a lengthy process of writing zeros across the entirety of:\n\n$selected_device_path" 15 60 && dd if=/dev/zero of=$selected_device_path bs=1M status=progress || echo "Let's continue then..."
 
-# Get encryption passphrase
 luks_pass1=$(dialog --no-cancel --passwordbox "Enter a passphrase for the LUKS encryption.\n\nMake it unique, and write it down." 10 60 3>&1 1>&2 2>&3 3>&1)
 luks_pass2=$(dialog --no-cancel --passwordbox "Retype the encryption passphrase." 10 60 3>&1 1>&2 2>&3 3>&1)
+
 while true; do
     [[ "$luks_pass1" != "" && "$luks_pass1" == "$luks_pass2" ]] && break
     luks_pass1=$(dialog --no-cancel --passwordbox "Uh oh! Your passphrases do not match. Try again." 10 60 3>&1 1>&2 2>&3 3>&1)
     luks_pass2=$(dialog --no-cancel --passwordbox "Retype the passphrase." 10 60 3>&1 1>&2 2>&3 3>&1)
 done
 
-# Get disk setup packages
 pacman -S parted cryptsetup lvm2 --noconfirm
 
-# Create partitions
 parted -s -a optimal $selected_device_path mklabel msdos
+
 parted -s -a optimal $selected_device_path mkpart primary fat32 1MiB 1GiB
 
-# Determine if the block device is nvme or not
 if [[ $selected_device_path =~ [0-9]$ ]]; then
     boot_partition="${selected_device_path}p1"
 else
     boot_partition="${selected_device_path}1"
 fi
 
-# Setup the boot partition
 mkfs.fat -F32 "$boot_partition"
 parted -s $selected_device_path set 1 boot on
+
 parted -s -a optimal $selected_device_path mkpart primary 1GiB 100%
 
-# Setup the encryption partition
 if [[ $selected_device_path =~ [0-9]$ ]]; then
     luks_partition="${selected_device_path}p2"
 else
+# Chroot into the new installation
     luks_partition="${selected_device_path}2"
 fi
 
-# Check if LUKS container already exists
 luks_container_exists=$(cryptsetup isLuks "$luks_partition" && echo "yes" || echo "no")
 
-# Prompt user to proceed to destroy extant LUKS setup or bail out
 if [ "$luks_container_exists" = "yes" ]; then
     dialog --defaultno --title "LUKS Container Exists" --yesno "\nYou have an extant LUKS superblock signature on ${luks_partition}.\n\nSelect < Yes > to abort installation.\n\nSelect < No > to proceed, allowing the installation process to remove it. This will take ~ 10s" 15 60 && exit || batch_mode_flag="-q"
+
     if cryptsetup status tankluks >/dev/null 2>&1; then
         echo "Removing existing tankluks mapping..."
         cryptsetup remove tankluks
@@ -108,7 +109,6 @@ if [ "$luks_container_exists" = "yes" ]; then
 else
     batch_mode_flag=""
 fi
-
 
 echo -n "$luks_pass1" | cryptsetup ${batch_mode_flag} luksFormat "$luks_partition" -
 echo -n "$luks_pass1" | cryptsetup open --type luks "$luks_partition" tankluks -
@@ -130,6 +130,7 @@ mount "$boot_partition" /mnt/boot
 lsblk -f
 sleep 10s
 
+# Bootstrap the base system
 basestrap /mnt base base-devel runit elogind-runit linux linux-firmware vim neovim grub btrfs-progs dosfstools brightnessctl htop cryptsetup lvm2 lvm2-runit efibootmgr
 
 echo "basestrap ran"
@@ -152,33 +153,42 @@ echo "tankluks UUID=$encrypted_partition_uuid none luks" >> /mnt/etc/crypttab
 cp barbs.sh /mnt/root/
 cp tank-programs.csv /mnt/root/
 
+# Chroot into the new system
 artix-chroot /mnt /bin/bash <<EOF
+# Set system hostname and locale
 
 echo "Still have Encrypted partition UUID: $encrypted_partition_uuid"
 echo "Still have LUKS container UUID: $luks_container_uuid"
 
 sleep 4s
 
+# Install and enable networking services
 sed -i 's/\(HOOKS=(.*block \)\(.*filesystems.*\))/\1encrypt lvm2 \2)/' /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
 sed -i "s|^GRUB_CMDLINE_LINUX_DEFAULT=\".*\"|GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 cryptdevice=UUID=$encrypted_partition_uuid:tankluks root=/dev/mapper/tankluks\"|" /etc/default/grub
+# Install and enable time synchronization services
 
 grub-install "$selected_device_path"
 grub-mkconfig -o /boot/grub/grub.cfg
+# Execute the post-installation script
+# Set root password, time, locale, and hostname
 
 echo "root:$rootpass1" | chpasswd
 
 hwclock --systohc
 
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
+# Setup network and time services
 echo "$hostname" > /etc/hostname
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
+# Install full xorg
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 export "LANG=en_US.UTF-8"
+# (Optional) Btrfs settings for Timeshift (currently commented out)
 echo "LC_COLLATE=C" >> /etc/locale.conf
 export LC_COLLATE="C"
 
@@ -188,13 +198,16 @@ ln -s /etc/runit/sv/NetworkManager/ /etc/runit/runsvdir/current
 pacman -S openntpd openntpd-runit --noconfirm
 ln -s /etc/runit/sv/openntpd/ /etc/runit/runsvdir/current
 
+# Execute barbs.sh script
 pacman -Sy xorg --noconfirm
 echo "full xorg install or reinstall"
+# Display post-installation message
 sleep 3s
 
 # btrfs quota enable /
 
 # sed -i \
+# Display ASCII art of tank
 #     -e 's/"do_first_run" : "true"/"do_first_run" : "false"/' \
 #     -e 's/"btrfs_mode" : "false"/"btrfs_mode" : "true"/' \
 #     -e 's/"include_btrfs_home" : "false"/"include_btrfs_home" : "true"/' \
